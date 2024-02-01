4e2c6a21d695b7f8d5a9efd94b916082323c6cd5
Jay Li <jayl@peplink.com>
2024-02-01 19:25:45 +0800

    Complete the dio exception

--- lib/services/dio/create_excpetion_entiry.dart
+++ lib/services/dio/create_excpetion_entiry.dart
@@ -6,39 +6,67 @@ ExceptionEntity createExceptionEntity(DioException exception) {
   switch (exception.type) {
     case DioExceptionType.connectionTimeout:
       return ExceptionEntity(
-          statusCode: -1, exceptionMessage: "Connection timed out [Dio]");
+        statusCode: -1,
+        exceptionMessage: "Connection timed out [Dio]",
+      );
 
     case DioExceptionType.sendTimeout:
       return ExceptionEntity(
-          statusCode: -2, exceptionMessage: "Send timed out [Dio]");
+        statusCode: -2,
+        exceptionMessage: "Send timed out [Dio]",
+      );
 
     case DioExceptionType.receiveTimeout:
       return ExceptionEntity(
-          statusCode: -3, exceptionMessage: "Receive timed out [Dio]");
+        statusCode: -3,
+        exceptionMessage: "Receive timed out [Dio]",
+      );
 
     case DioExceptionType.badCertificate:
       return ExceptionEntity(
-          statusCode: -4, exceptionMessage: "Bad SSL certificates [Dio]");
+        statusCode: -4,
+        exceptionMessage: "Bad SSL certificates [Dio]",
+      );
 
     case DioExceptionType.cancel:
       return ExceptionEntity(
-          statusCode: -5, exceptionMessage: "Server canceled [Dio]");
+        statusCode: -5,
+        exceptionMessage: "Server canceled [Dio]",
+      );
 
     case DioExceptionType.connectionError:
       return ExceptionEntity(
-          statusCode: -6, exceptionMessage: "Connection error [Dio]");
+        statusCode: -6,
+        exceptionMessage: "Connection error [Dio]",
+      );
 
     case DioExceptionType.badResponse:
       switch (exception.response!.statusCode) {
         case 400:
           return ExceptionEntity(
-              statusCode: 400,
-              exceptionMessage:
-                  "Bad request: incorrect data type or missing payload catch in Dio Interceptor");
+            statusCode: 400,
+            exceptionMessage: exception.response!.data['detail'],
+          );
         case 401:
           return ExceptionEntity(
-              statusCode: 401,
-              exceptionMessage: exception.response!.data['detail']);
+            statusCode: 401,
+            exceptionMessage: exception.response!.data['detail'],
+          );
+        case 403:
+          return ExceptionEntity(
+            statusCode: 403,
+            exceptionMessage: exception.response!.data['detail'],
+          );
+        case 405:
+          return ExceptionEntity(
+            statusCode: 405,
+            exceptionMessage: exception.response!.data['detail'],
+          );
+        case 409:
+          return ExceptionEntity(
+            statusCode: 409,
+            exceptionMessage: exception.response!.data['detail'],
+          );
         case 500:
           return ExceptionEntity(
               statusCode: 500,
