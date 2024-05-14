997161ac4a860c5394081a142cb4e93f1f0f35ea
Jay Li <jayl@peplink.com>
2024-05-14 16:20:08 +0800

    Working on get place image

--- lib/features/map/presentation/pages/google_map.dart
+++ lib/features/map/presentation/pages/google_map.dart
@@ -47,7 +47,18 @@ class _GoogleMapViewState extends State<GoogleMapView> {
 
   List allFavoritePlaces = [];
 
-  var tokenKey;
+  String tokenKey = '';
+
+  late PageController _pageController;
+  int prevPage = 0;
+  var tappedPlaceDetail;
+  String placeImg = '';
+  var photoGalleryIndex = 0;
+  bool showBlankCard = false;
+
+  final key = 'AIzaSyDvM7vtrGRyn3Ie3Fcpf0EJJ_8dN4WA4e8';
+
+  var selectedPlaceDetails;
 
   Set<Circle> _circles = Set<Circle>();
 
@@ -150,6 +161,37 @@ class _GoogleMapViewState extends State<GoogleMapView> {
     return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
         .buffer
         .asUint8List();
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.85)
+      ..addListener(_onScroll);
+    super.initState();
+  }
+
+  void _onScroll() {
+    if (_pageController.page!.toInt() != prevPage) {
+      prevPage = _pageController.page!.toInt();
+      cardTapped = false;
+      photoGalleryIndex = 1;
+      showBlankCard = false;
+      gotoTappedPlace();
+      fetchImage();
+    }
+  }
+
+  void fetchImage() async {
+    if (_pageController.page != null) {
+      if (allFavoritePlaces[_pageController.page!.toInt()]['photo'] != null) {
+        setState(() {
+          placeImg = allFavoritePlaces[_pageController.page!.toInt()]['photo']
+              [0]['photo_reference'];
+        });
+      } else {
+        placeImg = '';
+      }
+    }
   }
 
   @override
@@ -473,11 +515,28 @@ class _GoogleMapViewState extends State<GoogleMapView> {
                                             }
                                           });
                                         },
-                                        icon: Icon(Icons.timer_10_rounded))
+                                        icon: Icon(
+                                          Icons.mobile_friendly,
+                                          color: Colors.white,
+                                        ))
                               ],
                             ),
                           ),
                         )
+                      : Container(),
+                  pressedNear
+                      ? Positioned(
+                          bottom: 20.0,
+                          child: Container(
+                            height: 200.0,
+                            width: MediaQuery.of(context).size.width,
+                            child: PageView.builder(
+                                controller: _pageController,
+                                itemCount: allFavoritePlaces.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return _nearbyPlacesList(index);
+                                }),
+                          ))
                       : Container()
                 ])
               ],
@@ -535,6 +594,105 @@ class _GoogleMapViewState extends State<GoogleMapView> {
 
     _setMarker(LatLng(lat, lng));
     _setMarker(LatLng(endLat, endLng));
+  }
+
+  _nearbyPlacesList(index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget? widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = (_pageController.page! - index);
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 125.0,
+            width: Curves.easeInOut.transform(value) * 350.0,
+            child: widget,
+          ),
+        );
+      },
+      child: InkWell(
+        onTap: () async {
+          cardTapped = !cardTapped;
+        },
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                height: 125.0,
+                width: 275.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black54,
+                          offset: Offset(0.0, 4.0),
+                          blurRadius: 10.0)
+                    ]),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white),
+                  child: Row(children: [
+                    _pageController.position.haveDimensions
+                        ? _pageController.page!.toInt() == index
+                            ? Container(
+                                height: 90.0,
+                                width: 90.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(10.0),
+                                        topLeft: Radius.circular(10.0)),
+                                    image: DecorationImage(
+                                        image: NetworkImage(placeImg != ''
+                                            ? 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=$placeImg&key=$key'
+                                            : 'https://pic.onlinewebfonts.com/svg/img_546302.png'),
+                                        fit: BoxFit.cover)),
+                              )
+                            : Container(
+                                height: 90.0,
+                                width: 20.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10.0),
+                                      topLeft: Radius.circular(10.0),
+                                    ),
+                                    color: Colors.blue),
+                              )
+                        : Container(),
+                  ]),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> gotoTappedPlace() async {
+    final GoogleMapController controller = await _controller.future;
+
+    _markers = {};
+
+    var selectedPlace = allFavoritePlaces[_pageController.page!.toInt()];
+
+    _setNearMarker(
+        LatLng(selectedPlace['geometry']['location']['lat'],
+            selectedPlace['geometry']['location']['lng']),
+        selectedPlace['name'] ?? 'no name',
+        selectedPlace['types'],
+        selectedPlace['business_status'] ?? 'none');
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(selectedPlace['geometry']['location']['lat'],
+            selectedPlace['geometry']['location']['lng']),
+        zoom: 14.0,
+        bearing: 45.0,
+        tilt: 45.0)));
   }
 
   Future<void> gotoSearchedPlace(double lat, double lng) async {
